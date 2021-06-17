3b9c976e119ee56d2e9aaf5e559deda01c5fef34
rokl <richard@kovar.co>
2021-06-17 11:54:16 -0400

    Implementing more testing

--- src/bullmq.connector.ts
+++ src/bullmq.connector.ts
@@ -2,8 +2,8 @@
 const Connector = require('loopback-connector').Connector;
 
 export class BullMQConnector extends Connector {
-    constructor(name: string, settings: any) {
+    constructor(settings: any) {
         console.log(settings);
-        super(name, settings);
+        super(settings);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { BullMQConnector } from './bullmq.connector';
 
 export function initialize(dataSource: { settings: any, connector: any }, cb: () => void) {
-    const connector = new BullMQConnector('BullMQ', dataSource.settings);
+    const connector = new BullMQConnector(dataSource.settings);
+    console.log(dataSource);
     dataSource.connector = connector;
     dataSource.connector.dataSource = dataSource;
-    console.log('BullMQ Connector Initialized')
     cb();
 }
